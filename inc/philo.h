cf2c693f90b7709003f7760c62a0fef2adb0fc8e
rhernand <rhernand@student.42malaga.com>
2025-03-09 20:54:58 +0100

    args stored and checked

--- inc/philo.h
+++ inc/philo.h
@@ -6,22 +6,29 @@
 /*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 19:00:22 by rhernand          #+#    #+#             */
-/*   Updated: 2025/03/09 19:06:56 by rhernand         ###   ########.fr       */
+/*   Updated: 2025/03/09 20:35:11 by rhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 # include "libft/inc/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <stdint.h>
 
 typedef struct s_philo
 {
-	/* data */
+	int	id;
 }		t_philo;
 
 typedef struct s_data
 {
-	/* data */
+	int			n_philo;
+	uint64_t	ttd;
+	uint64_t	tte;
+	uint64_t	tts;
+	int			n_meals;
 }		t_data;
 
 #endif
---
+++ makefile
@@ -0,0 +1,4 @@
+debug:
+	gcc src/*.c inc/libft/libft.a -g -o philo
+
+PHONY: debug
