f1d1f1ea351cec47e4633d0b1440bfe8a9adad59
rhernand <rhernand@student.42malaga.com>
2025-03-09 19:22:31 +0100

    add libft

---
+++ inc/philo.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/09 19:00:22 by rhernand          #+#    #+#             */
+/*   Updated: 2025/03/09 19:06:56 by rhernand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include "libft/inc/libft.h"
+
+typedef struct s_philo
+{
+	/* data */
+}		t_philo;
+
+typedef struct s_data
+{
+	/* data */
+}		t_data;
+
+#endif
